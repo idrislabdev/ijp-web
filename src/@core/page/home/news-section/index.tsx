5af08cec8664486d85abcdc8d0623212fd3bb1a7
mochamad rifai idris <inoreds@gmail.com>
2024-08-23 08:03:57 +0700

    update

---
+++ src/@core/page/home/news-section/index.tsx
@@ -0,0 +1,104 @@
+import Image from 'next/image'
+import React from 'react'
+import '@/styles/components/cards.css'
+
+const HomeNewsSection = (props: {lang:any}) => {
+    const { lang } = props
+    return (
+        <section className='home-news-section'>
+            <div className='home-news-container'>
+                <div className='news-header'>
+                    <div className='news-title'>
+                        <h1>Berita Terkini</h1>
+                        <p>Cras a elit sit amet leo accumsan volutpat. Suspendisse hendrerit vehicula leo, vel efficitur felis ultrices non. Integer aliquet ullamcorper dolor, quis sollicitudin. Integer aliquet ullamcorper dolor, quis sollicitudin</p>
+                    </div>
+                    <a>Berita Lainnya</a>
+                </div>
+                <div className='news-subcontainer'>
+                    <div className='card-news'>
+                        <Image src='/images/news/blog-big-2.jpg' alt='blog news image' width="0" height="0" sizes="100%" />
+                        <div className='card-body'>
+                            <div className='text-date'>
+                                <label>21 Agustus 2024</label>
+                            </div>
+                            <div className='text-title'>
+                                <h5><a>Lorem ipsum dolor sit amet</a></h5>
+                            </div>
+                            <div className='text-content'>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc viverra lorem , 
+                                    consectetur adipiscing elit...
+                                </p>
+                            </div>
+                            <div className='text-readmore'>
+                                <a>Lebih Lengkap</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='card-news'>
+                        <Image src='/images/news/blog-big-2.jpg' alt='blog news image' width="0" height="0" sizes="100%"/>
+                        <div className='card-body'>
+                            <div className='text-date'>
+                                <label>21 Agustus 2024</label>
+                            </div>
+                            <div className='text-title'>
+                                <h5><a>Lorem ipsum dolor sit amet</a></h5>
+                            </div>
+                            <div className='text-content'>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc viverra lorem , 
+                                    consectetur adipiscing elit...
+                                </p>
+                            </div>
+                            <div className='text-readmore'>
+                                <a>Lebih Lengkap</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='card-news'>
+                        <Image src='/images/news/blog-big-2.jpg' alt='blog news image' width="0" height="0" sizes="100%"/>
+                        <div className='card-body'>
+                            <div className='text-date'>
+                                <label>21 Agustus 2024</label>
+                            </div>
+                            <div className='text-title'>
+                                <h5><a>Lorem ipsum dolor sit amet</a></h5>
+                            </div>
+                            <div className='text-content'>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc viverra lorem , 
+                                    consectetur adipiscing elit...
+                                </p>
+                            </div>
+                            <div className='text-readmore'>
+                                <a>Lebih Lengkap</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='card-news'>
+                        <Image src='/images/news/blog-big-2.jpg' alt='blog news image' width="0" height="0" sizes="100%"/>
+                        <div className='card-body'>
+                            <div className='text-date'>
+                                <label>21 Agustus 2024</label>
+                            </div>
+                            <div className='text-title'>
+                                <h5><a>Lorem ipsum dolor sit amet</a></h5>
+                            </div>
+                            <div className='text-content'>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc viverra lorem , 
+                                    consectetur adipiscing elit...
+                                </p>
+                            </div>
+                            <div className='text-readmore'>
+                                <a>Lebih Lengkap</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default HomeNewsSection
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -4,6 +4,7 @@ import '@/styles/home.css'
 import HomeAboutSection from '@/@core/page/home/about-section'
 import HomeBusinessUnitSection from '@/@core/page/home/business-unit-section'
 import HomeBannerSection from '@/@core/page/home/banner-section'
+import HomeNewsSection from '@/@core/page/home/news-section'
 
 export default async function  MainPage({ params }: any) {
   return (
@@ -12,6 +13,7 @@ export default async function  MainPage({ params }: any) {
       <HomeAboutSection objLang={null} />
       <HomeBusinessUnitSection objLang={null}/>
       <HomeOurProductsSection />
+      <HomeNewsSection objLang={null}/>
     </main>
   )
 }
