136c1ffa4f7d3f2948c38eab89910f2319cc604b
mochamad rifai idris <inoreds@gmail.com>
2024-09-23 09:50:49 +0700

    update

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import {  usersRepo } from '@/@core/helpers/users-repo';
 import { promises as fs } from 'fs';
 
+import dataUsers from '@/app/data/users.json'
 
 // export async function POST(req: NextRequest) {
 //     try {
@@ -45,10 +46,10 @@ export async function POST(req: Request) {
 
 export async function GET() {
     try {
-        let file_data = await fs.readFile(process.cwd() + '/src/app/data/users.json', 'utf8');
-        let users = JSON.parse(file_data)
+        // let file_data = await fs.readFile(process.cwd() + '/src/app/data/users.json', 'utf8');
+        // let users = JSON.parse(file_data)
 
-        return NextResponse.json({ status: "success", user:users});
+        return NextResponse.json({ status: "success", user:dataUsers});
     } catch (e) {
       console.error(e);
       return NextResponse.json({ status: "fail", error: e });
