0f23890a55b1044b5f4ea58d08045ae2823d833e
mochamad rifai idris <inoreds@gmail.com>
2024-11-15 05:43:35 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomePageWrapper from '@/@core/page/home/page-wrapper'
+import Footer from '@/@core/components/footer'
 
 export default async function  MainPage({ params }: any) {
   return (
     <>
       <HomePageWrapper lang={'id'} />
+      <Footer />
     </>
   )
 }
