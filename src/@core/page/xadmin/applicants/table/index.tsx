3a41ff3605c2efd21dfb0fc5db40567303e07d26
mochamad rifai idris <inoreds@gmail.com>
2024-11-15 04:22:42 +0700

    update

--- src/@core/page/xadmin/applicants/table/index.tsx
+++ src/@core/page/xadmin/applicants/table/index.tsx
@@ -6,13 +6,16 @@ import axiosInstance from '@/@core/utils/axios';
 import { Message, useToaster } from 'rsuite';
 import ModalApplicant from '../modal-applicant';
 import ModalConfirm from '@/@core/components/modal/modal-confirm';
+import * as XLSX from "xlsx";
 
 const XadminApplicantsTable = () => {
     const [applicants, setApplicants] = useState([]);
+    const [offices, setOffices] = useState([]);
     const [applicant, setApplicant] = useState({})
     const [openModal, setOpenModal ] = useState(false);
     const [openModalConfirm, setOpenModalConfirm ] = useState(false);
     const [selectedId, setSelectedId] = useState('');
+    const [selectPosition, setSelectPosition] = useState('Semua')
 
     const toaster = useToaster();
 
@@ -32,20 +35,60 @@ const XadminApplicantsTable = () => {
     const confirmDelete = async () => {
         const response = await axiosInstance.delete(`/api/jobs-applicant/${selectedId}`);
         const { data } = response.data
-        await getData();
+        await getData('');
         await setOpenModalConfirm(false)
         await toaster.push(message, { placement:'bottomEnd', duration: 5000 })
     }
-    const getData = async () => {
+    const getData = async (filter:string) => {
         const response = await axiosInstance.get(`/api/jobs-applicant`);
         const { data } = response.data
-        setApplicants(data)
+        if (filter === '') {
+            setApplicants(data)
+        } else {
+            setApplicants(data.filter((x:any) => x.job_location === filter))
+        }
     }
+
+    const getDataOffices = async () => {
+        const response = await axiosInstance.get(`/api/offices`);
+        const { data } = response.data
+        setOffices(data)
+    }
+
+    const handleChange = (e: React.MouseEvent<HTMLDivElement, MouseEvent> | any) => {
+        setSelectPosition(e.target.value)
+        getData(e.target.value)
+    }
+
+    const exportData = async () => {
+        const dataToExport = applicants.map((item: any) => ({
+            'Nama Pelamar': item.name,
+            'Judul Pekerjaan': item.job_name,
+            'Posisi Pekerjaan': item.job_position,
+            'Lokasi Pekerjaann': item.job_location,
+          }),);
+          const workbook = XLSX.utils.book_new();
+          const worksheet = XLSX.utils?.json_to_sheet(dataToExport);
+          XLSX.utils.book_append_sheet(workbook, worksheet, 'daftar pelamar');
+          // Save the workbook as an Excel file
+          XLSX.writeFile(workbook, `daftar_pelamar.xlsx`)
+    }
+    
     useEffect(() => {
-        getData()
+        getData('')
+        getDataOffices()
     }, [])
     return (
         <>
+            <div className='flex justify-end gap-[4px]'>
+                <select className='h-[40px]' onChange={handleChange} value={selectPosition}>
+                    <option value={''}>Semua</option>
+                    {offices.map((item:any, index:number) => (
+                        <option value={item.name} key={index}>{item.name}</option>
+                    ))}
+                </select>
+                <button className='btn btn-primary btn-big' onClick={() => exportData()}>Download Data</button>
+            </div>
             <table>
                 <thead>
                     <tr>
