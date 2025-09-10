baa7ab5ba05687bb41fd31453f14909280a3fbb7
mochamad <inoreds@gmail.com>
2025-09-10 10:07:43 +0700

    progress: update

--- src/@core/components/footer/index.tsx
+++ src/@core/components/footer/index.tsx
@@ -1,66 +1,103 @@
-import React from 'react'
-import '@/styles/components/footer.css'
-import Image from 'next/image'
-import offices from "@/app/data/offices.json"
+import React from "react";
+import "@/styles/components/footer.css";
+import Image from "next/image";
+import offices from "@/app/data/offices.json";
 
-const Footer = () => {
+const Footer = (props: { lang: string }) => {
+  const { lang } = props;
   return (
-    <div className='footer sm:mobile-responsive'>
-        <div className='footer-primary'>
-            <div className='footer-main-info'>
-                <div className='image-logo-container'>
-                    <Image src='/images/logoes/logo-unicorp-big.png' alt='logo unicorp' width={128} height={36}/>
-                </div>
-                <div className='main-address-container'>
-                    <h5>{offices.find((x) => x.type == 'main')?.name}</h5>
-                    <p>{offices.find((x) => x.type == 'main')?.address}</p>
-                    <p>No. Telp <span>{offices.find((x) => x.type == 'main')?.phones.join(", ")}</span></p>
-                </div>
-                <div className='registered-by-container'>
-                    <h5>Terdaftar Dan Tersertifikasi Oleh</h5>
-                    <div className='certifieds-subcontainer'>
-                        <Image src='/images/institutions/halal-mui.png' className='logo-halal' alt='halal mui' width={0} height={0} sizes='100%'/>
-                        <Image src='/images/institutions/bpom.png' className='logo-bpom' alt='mui' width={0} height={0} sizes='100%'/>
-                    </div>
-                    <div className='certifieds-subcontainer'>
-                        <Image src='/images/institutions/logo-iso.png' className='logo-iso' alt='iso bspji' width={320} height={320}/>
-                    </div>
-                </div>
+    <div className="footer sm:mobile-responsive">
+      <div className="footer-primary">
+        <div className="footer-main-info">
+          <div className="image-logo-container">
+            <Image
+              src="/images/logoes/logo-unicorp-big.png"
+              alt="logo unicorp"
+              width={128}
+              height={36}
+            />
+          </div>
+          <div className="main-address-container">
+            <h5>{lang === "en" ? "Head Office" : "Kantor Pusat"}</h5>
+            <p>{offices.find((x) => x.type == "main")?.address}</p>
+            <p>
+              {lang === "en" ? "No. Telp" : "No. Telp"}:
+              <span>
+                {offices.find((x) => x.type == "main")?.phones.join(", ")}
+              </span>
+            </p>
+          </div>
+          <div className="registered-by-container">
+            <h5>
+              {lang === "en"
+                ? "Registered and Certified By"
+                : "Terdaftar Dan Tersertifikasi Oleh"}
+              :
+            </h5>
+            <div className="certifieds-subcontainer">
+              <Image
+                src="/images/institutions/halal-mui.png"
+                className="logo-halal"
+                alt="halal mui"
+                width={0}
+                height={0}
+                sizes="100%"
+              />
+              <Image
+                src="/images/institutions/bpom.png"
+                className="logo-bpom"
+                alt="mui"
+                width={0}
+                height={0}
+                sizes="100%"
+              />
             </div>
-            <div className='footer-address'>
-                {offices.filter((x) => x.type === 'branch').map((item:any, index:number) => (
-                    <div className='address-container' key={index}>
-                        <h5>{item.name}</h5>
-                        <div className='address-subcontainer'>
-                            <div className='detail-adress'>
-                                <h6>Alamat:</h6>
-                                <p>{item.address}</p>
-                            </div>
-                            <div className='detail-adress'>
-                                <h6>No. Telp:</h6>
-                                {item.phones.map((phone:any, index2:number) => (
-                                    <p key={index2}>{phone}</p>
-
-                                ))}
-                            </div>
-                            {item.faxs.length > 0 && 
-                                <div className='detail-adress'>
-                                    <h6>Fax:</h6>
-                                    {item.faxs.map((fax:any, index3:number) => (
-                                        <p key={index3}>{fax}</p>
-                                    ))}
-                                </div>
-                            }
-                        </div>
-                    </div>
-                ))}
+            <div className="certifieds-subcontainer">
+              <Image
+                src="/images/institutions/logo-iso.png"
+                className="logo-iso"
+                alt="iso bspji"
+                width={320}
+                height={320}
+              />
             </div>
+          </div>
         </div>
-        <div className='footer-copyright'>
-            <p>© 2021 - 2024 Indo Jaya Putra All Rights Reserved.</p>
+        <div className="footer-address">
+          {offices
+            .filter((x) => x.type === "branch")
+            .map((item: any, index: number) => (
+              <div className="address-container" key={index}>
+                <h5>{lang === "en" ? item.name_en : item.name}</h5>
+                <div className="address-subcontainer">
+                  <div className="detail-adress">
+                    <h6>{lang === "en" ? "Address" : "Alamat"}:</h6>
+                    <p>{item.address}</p>
+                  </div>
+                  <div className="detail-adress">
+                    <h6>{lang === "en" ? "No. Telp" : "No. Telp"}:</h6>
+                    {item.phones.map((phone: any, index2: number) => (
+                      <p key={index2}>{phone}</p>
+                    ))}
+                  </div>
+                  {item.faxs.length > 0 && (
+                    <div className="detail-adress">
+                      <h6>{lang === "en" ? "Fax Number" : "No. Fax"}:</h6>
+                      {item.faxs.map((fax: any, index3: number) => (
+                        <p key={index3}>{fax}</p>
+                      ))}
+                    </div>
+                  )}
+                </div>
+              </div>
+            ))}
         </div>
+      </div>
+      <div className="footer-copyright">
+        <p>© 2021 - 2025 Indo Jaya Putra All Rights Reserved.</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,16 +1,22 @@
-import MainHeader from '@/@core/components/main-header'
-import React from 'react'
-import '@/styles/components/main-header.css'
-import Footer from '@/@core/components/footer'
+import MainHeader from "@/@core/components/main-header";
+import React from "react";
+import "@/styles/components/main-header.css";
+import Footer from "@/@core/components/footer";
 
-const MainLayout = ({ children, params }: {children : React.ReactNode, params: any}) => {
-    return (
-        <>
-            <MainHeader lang={params.lang}/>
-            { children }
-            <Footer />
-        </>
-    )
-  }
-  
-  export default MainLayout
+const MainLayout = ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: any;
+}) => {
+  return (
+    <>
+      <MainHeader lang={params.lang} />
+      {children}
+      <Footer lang={params.lang} />
+    </>
+  );
+};
+
+export default MainLayout;
