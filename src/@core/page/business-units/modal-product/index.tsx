079fef8350e741d2db4a90fa9be9ac7ba88c093b
mochamad <inoreds@gmail.com>
2025-09-18 18:30:39 +0700

    update

--- src/@core/page/business-units/modal-product/index.tsx
+++ src/@core/page/business-units/modal-product/index.tsx
@@ -1,39 +1,60 @@
-import { AddOutlineIcon } from '@/@core/my-icons';
-import axiosInstance from '@/@core/utils/axios';
-import moment from 'moment'
-import Image from 'next/image';
-import React, { Dispatch, SetStateAction, useCallback, useEffect, useState } from 'react'
-import { Message, Modal, useToaster } from 'rsuite'
-import '@/styles/components/cards.css'
+import { AddOutlineIcon } from "@/@core/my-icons";
+import axiosInstance from "@/@core/utils/axios";
+import moment from "moment";
+import Image from "next/image";
+import React, {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
+import { Message, Modal, useToaster } from "rsuite";
+import "@/styles/components/cards.css";
 
 const ModalProduct = (props: {
-        isModalOpen:boolean, 
-        setIsModalOpen: Dispatch<SetStateAction<boolean>>, 
-        product:any
-    }) => {
-    const { isModalOpen, setIsModalOpen, product } = props
-    const [products, setProducts] = useState([])
+  isModalOpen: boolean;
+  setIsModalOpen: Dispatch<SetStateAction<boolean>>;
+  product: any;
+  lang: string;
+}) => {
+  const { isModalOpen, setIsModalOpen, product, lang } = props;
+  const [products, setProducts] = useState([]);
 
+  return (
+    <Modal
+      size="lg"
+      className="modal-product view"
+      backdropClassName="my-modal-backdrop"
+      backdrop="static"
+      keyboard={true}
+      open={isModalOpen}
+      onClose={(_) => setIsModalOpen(false)}
+    >
+      <Modal.Header></Modal.Header>
+      <Modal.Body>
+        {product && (
+          <div className="flex gap-[10px] mb-[10px]">
+            <Image
+              src={product.image_url}
+              className="our-product-img"
+              alt="unicol"
+              width={350}
+              height={350}
+            />
+            <div className="flex flex-col gap-[10px] w-[50%]">
+              <h2>{product[lang].name}</h2>
+              <hr />
+              <h5>{product[lang].description}</h5>
+              <p>{product[lang].full_description_1}</p>
+              <p>{product[lang].full_description_2}</p>
+              <p>{product[lang].full_description_3}</p>
+            </div>
+          </div>
+        )}
+      </Modal.Body>
+    </Modal>
+  );
+};
 
-    return (
-        <Modal size="lg" className='modal-product view' backdropClassName="my-modal-backdrop" backdrop="static" keyboard={true} open={isModalOpen} onClose={_ => setIsModalOpen(false)}>
-            <Modal.Header>
-            </Modal.Header>
-            <Modal.Body>
-                <div className='flex gap-[10px] mb-[10px]'>
-                    <Image src={product?.image_url} className='our-product-img' alt='unicol' width={350} height={350} />
-                    <div className='flex flex-col gap-[10px] w-[50%]'>
-                        <h2>{product?.name}</h2>
-                        <hr />
-                        <h5>{product?.description}</h5>
-                        <p>{product?.full_description_1}</p>
-                        <p>{product?.full_description_2}</p>
-                        <p>{product?.full_description_3}</p>
-                    </div>
-                </div>
-            </Modal.Body>
-        </Modal>
-  )
-}
-
-export default ModalProduct
+export default ModalProduct;
--- src/@core/page/business-units/products-section/index.tsx
+++ src/@core/page/business-units/products-section/index.tsx
@@ -1,67 +1,95 @@
-"use client"
+"use client";
 
-import Image from 'next/image'
-import React, { useState } from 'react'
-import ModalProduct from '../modal-product'
+import Image from "next/image";
+import React, { useState } from "react";
+import ModalProduct from "../modal-product";
 
-const BusinessUnitsProductsSection = (props: {objLang:any, lang:string}) => {
-    const { objLang, lang } = props
-    const [product, setProduct] = useState(null)
-    const [openModal, setOpenModal ] = useState(false);
+const BusinessUnitsProductsSection = (props: {
+  objLang: any;
+  lang: string;
+}) => {
+  const { objLang, lang } = props;
+  const [product, setProduct] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
 
-    const showProduct = (obj:any) => {
-        setProduct(obj)
-        setOpenModal(true)
-    }
-    return (
-        <>
-            <section className='business-units-products-section'>
-                <div className='products-container'>
-                    <div className='products-title'>
-                        <h1>{objLang[lang].title}</h1>
-                        <p>{objLang[lang].description}</p>
+  const showProduct = (obj: any) => {
+    setProduct(obj);
+    setOpenModal(true);
+  };
+  return (
+    <>
+      <section className="business-units-products-section">
+        <div className="products-container">
+          <div className="products-title">
+            <h1>{objLang[lang].title}</h1>
+            <p>{objLang[lang].description}</p>
+          </div>
+          <div className="product-detail-container">
+            <h5 className="text-2xl font-semibold">
+              {lang === "id" ? "Produk Internal" : "Internal Products"}
+            </h5>
+            <div className="products-subcontainer">
+              {objLang.products.map((item: any, index: number) => (
+                <div className="our-product-wrapper" key={index}>
+                  <div
+                    className="our-product-card"
+                    onClick={(_) => showProduct(item)}
+                  >
+                    <Image
+                      src={item.image_url}
+                      className="our-product-img"
+                      alt="unicol"
+                      width={0}
+                      height={0}
+                      sizes="100%"
+                    />
+                    <div className="card-overlay">
+                      <h6>{item.name}</h6>
+                      <label>{item[lang].description}</label>
                     </div>
-                    <div className='product-detail-container'>
-                        <h5 className='text-2xl font-semibold'>Produk Internal</h5>
-                        <div className='products-subcontainer'>
-                            {objLang.products.map((item:any, index:number) => (
-                                <div className='our-product-wrapper' key={index}>
-                                    <div className='our-product-card' onClick={_ => showProduct(item)}>
-                                        <Image src={item.image_url} className='our-product-img' alt='unicol' width={0} height={0} sizes='100%'/>
-                                        <div className='card-overlay'>
-                                            <h6>{item.name}</h6>
-                                            <label>{item.description}</label>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                    <div className='product-detail-container'>
-                        <h5 className='text-2xl font-semibold'>Produk Lainnya</h5>
-                        <div className='products-subcontainer'>
-                            {objLang.products_others.map((item:any, index:number) => (
-                                <div className='our-product-wrapper' key={index}>
-                                    <div className='our-product-card' onClick={_ => showProduct(item)}>
-                                        <Image src={item.image_url} className='our-product-img' alt='unicol' width={0} height={0} sizes='100%'/>
-                                        <div className='card-overlay'>
-                                            <h6>{item.name}</h6>
-                                            <label>{item.description}</label>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
+                  </div>
                 </div>
-            </section>
-            <ModalProduct
-                isModalOpen={openModal} 
-                setIsModalOpen={setOpenModal} 
-                product={product}
-            />
-        </>
-    )
-}
+              ))}
+            </div>
+          </div>
+          <div className="product-detail-container">
+            <h5 className="text-2xl font-semibold">
+              {lang === "id" ? "Produk Lainnya" : "Other Products"}
+            </h5>
+            <div className="products-subcontainer">
+              {objLang.products_others.map((item: any, index: number) => (
+                <div className="our-product-wrapper" key={index}>
+                  <div
+                    className="our-product-card"
+                    onClick={(_) => showProduct(item)}
+                  >
+                    <Image
+                      src={item.image_url}
+                      className="our-product-img"
+                      alt="unicol"
+                      width={0}
+                      height={0}
+                      sizes="100%"
+                    />
+                    <div className="card-overlay">
+                      <h6>{item.name}</h6>
+                      <label>{item[lang].description}</label>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+      <ModalProduct
+        isModalOpen={openModal}
+        setIsModalOpen={setOpenModal}
+        product={product}
+        lang={lang}
+      />
+    </>
+  );
+};
 
-export default BusinessUnitsProductsSection
+export default BusinessUnitsProductsSection;
