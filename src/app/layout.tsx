9534c8393c2c10536882ccd497418be8e177152f
mochamad <inoreds@gmail.com>
2025-09-25 12:07:05 +0700

    uypdate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,17 @@ import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import "@/styles/components/forms.css";
 import "@/styles/components/modals.css";
-import 'rsuite/Message/styles/index.css';
-import 'rsuite/useToaster/styles/index.css';
-import 'rsuite/Modal/styles/index.css';
-import 'rsuite/Animation/styles/index.css';
+import "rsuite/Message/styles/index.css";
+import "rsuite/useToaster/styles/index.css";
+import "rsuite/Modal/styles/index.css";
+import "rsuite/Animation/styles/index.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "UNICORP | Grow Through Innovation",
-  description: "UNICORP adalah Grup perusahaan yang bergerak di berbagai bidang industri, diantaranya: Makanan, Pertanian, Retail serta Industri lainnya",
+  title: "UNICORP | Growth Through Innovation",
+  description:
+    "UNICORP adalah Grup perusahaan yang bergerak di berbagai bidang industri, diantaranya: Makanan, Pertanian, Retail serta Industri lainnya",
 };
 
 export default function RootLayout({
